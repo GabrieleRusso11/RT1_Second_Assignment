2eb2b5e3eea8e2c1215913078f2d0526be4dabf8
GabrieleRusso11 <93495918+GabrieleRusso11@users.noreply.github.com>
2021-12-20 18:58:08 +0100

    Add files via upload

--- robot_controller/src/controller.cpp
+++ robot_controller/src/controller.cpp
@@ -10,20 +10,24 @@ float min_r_f = 100;
 float min_f = 100;
 float min_r = 100;
 float min_l = 100;
-float ranges[720], right_side[240], left_side[240];
-float left_front[80], right_front[80], front[80];
-float th = 2.0;
-float th_go = 1.0;
-float dan = 0.5;
-float th_s = 0.6;
+
+float ranges[720], right_side[320], left_side[320], front[80];
+
+
+//float th_go = 1.0;
+float dan = 0.3;
+float th_s = 0.4;
+float th_min = 0.25;
 float th_f = 1.9;
+float speed = 5.0;
+float th = 2;
 
 
 ros::Publisher pub;
 
 float min_right_side(float * f){
 	int j = 0;
-	for(int i = 0; i < 240; i++){
+	for(int i = 0; i < 320; i++){
 		right_side[j] = f[i];
 		if(right_side[j] < min_r){
 			min_r = right_side[j];
@@ -35,7 +39,7 @@ float min_right_side(float * f){
 
 float min_left_side(float * f){
 	int j = 0;
-	for(int i = 480; i < 720; i++){
+	for(int i = 400; i < 720; i++){
 		left_side[j] = f[i];
 		if(left_side[j] < min_l){
 			min_l = left_side[j];
@@ -59,44 +63,23 @@ float min_front(float * f){
 
 }
 
-float min_left_front(float * f){
-
-	int j=0;
-	for(int i = 400; i < 480; i++){
-
-		left_front[j] = f[i];
-		//cout<<"left"<<left_front[j]<<endl;
-		if(left_front[j] < min_l_f){
-			min_l_f = left_front[j];
-		}
-		j++;
-	}
-	return min_l_f;
-
-}
-
-float min_right_front(float * f){
-
-	int j=0;
-	for(int i = 240; i < 320; i++){
-		right_front[j] = f[i];
-		//cout<<"right"<<right_front[j]<<endl;
-		if(right_front[j] < min_r_f){
-			min_r_f = right_front[j];
-		}
-		j++;
-	}
-	return min_r_f;
-}
-
-
-
 void drive(float speed_l, float speed_a){
 	geometry_msgs::Twist robot_velocity;
 
 	
 	robot_velocity.linear.x = speed_l;
 	robot_velocity.angular.z = speed_a;
+
+	pub.publish(robot_velocity);
+
+}
+
+void turn(float speed){
+	geometry_msgs::Twist robot_velocity;
+
+	
+	robot_velocity.linear.x = 0;
+	robot_velocity.angular.z = speed;
 
 	pub.publish(robot_velocity);
 
@@ -113,113 +96,115 @@ void stop(){
 
 }
 
-
 void avoid_obstacle(float * r){
 	if(min_front(r) > th && min_left_side(r) > th_s && min_right_side(r) > th_s){
-		drive(10.0,0.0);
-	}
-	else if(min_front(r) < th_go){
-		if(min_left_side(r) < min_right_side(r) && min_left_side(r) < 0.4){
-			cout<<"turn right"<<endl;
-			drive(0.8,0.9);
-			
-		}
-		else if(min_right_side(r) < min_left_side(r) && min_right_side(r) < 0.4){
-			cout<<"turn left"<<endl;
-			drive(0.8,-0.9);
-		}
-		else{
-			cout<<"turn left"<<endl;
-			drive(0.8,-0.9);
-		}
-	}
-	else if(min_front(r) < dan){
-		if(min_left_side(r) < min_right_side(r)){
-			cout<<"turn right"<<endl;
-			drive(0.0,0.9);
-			if(front[40] > th_f){
-				cout<<"go on"<<endl;
-				drive(0.7,0.0);
-			}
-		}
-		else if(min_right_side(r) < min_left_side(r)){
-			cout<<"turn left"<<endl;
-			drive(0.0,-0.9);
-			if(front[40] > th_f){
-				cout<<"go on"<<endl;
-				drive(0.7,0.0);
-			}
-		}
-		else{
-			drive(0.0,-0.9);
-			if(front[40] > th_f){
-				cout<<"go on"<<endl;
-				drive(0.7,0.0);
-			}
-		}
-	}
-	
-	/*else if(th_go < min_front(r) < th && min_left_side(r) > th_s && min_right_side(r) > th_s){
-		drive(0.8,0.0);
-		cout<<"ciao"<<endl;
-	}	*/
-	else if(min_front(r) > th && min_left_side(r) > th_s && min_right_side(r) < th_s){
-		cout<<"turn left"<<endl;
-		drive(0.0,0.9);
+		drive(speed,0.0);
 	}
 	else if(min_front(r) > th && min_left_side(r) < th_s && min_right_side(r) > th_s){
-		cout<<"turn right"<<endl;
-		drive(0.0,-0.9);
+		cout<<"i'm near to a wall on the left"<<endl;
+		drive(speed,-speed);
 	}
-	else{
-		drive(0.7,0.0);
-		cout<<"go on slowly"<<endl;
+	else if(min_front(r) > th && min_left_side(r) > th_s && min_right_side(r) < th_s){
+		cout<<"i'm near to a wall on the right"<<endl;
+		drive(speed,speed);
+	}
+	else if(min_front(r) < th && min_left_side(r) > th_s && min_right_side(r) > th_s){
+		if(min_front(r) < dan){
+			stop();
+			cout<<"danger"<<endl;
+			if(min_left_side(r) < min_right_side(r)){
+				cout<<"stop and turn right"<<endl;
+				turn(-0.8);
+				if(front[35] > th_f && front[40] > th_f && front[45] > th_f){
+					cout<<"go on"<<endl;
+					drive(0.7,0.0);
+				}
+			}
+			else{
+				cout<<"stop and turn left"<<endl;
+				turn(0.8);
+				if(front[35] > th_f && front[40] > th_f && front[45] > th_f){
+					cout<<"go on"<<endl;
+					drive(0.7,0.0);
+				}
+			}
+		}
+		else{
+			drive(speed,0.0);
+		}
+	}
+	else if(min_front(r) < th && min_left_side(r) < th_s && min_right_side(r) > th_s){
+		if(min_front(r) < dan){
+			stop();
+			cout<<"danger"<<endl;
+			cout<<"stop and turn right"<<endl;
+			turn(-1.0);
+			if(front[35] > th_f && front[40] > th_f && front[45] > th_f){
+				cout<<"go on"<<endl;
+				drive(0.7,0.0);
+			}
+		}
+		else{
+			cout<<"turn right"<<endl;
+			drive(0.7,-1.0);
+		}
+	}
+	else if(min_front(r) < th && min_left_side(r) > th_s && min_right_side(r) < th_s){
+		if(min_front(r) < dan){
+			stop();
+			cout<<"danger"<<endl;
+			cout<<"stop and turn left"<<endl;
+			turn(1.0);
+			if(front[35] > th_f && front[40] > th_f && front[45] > th_f){
+				cout<<"go on"<<endl;
+				drive(0.7,0.0);
+			}
+		}
+		else{
+			cout<<"turn left"<<endl;
+			drive(0.7,1.0);
+		}
+	}
+	else if(min_front(r) < th && min_left_side(r) < th_s && min_right_side(r) < th_s){
+		if(min_front(r) < dan){
+			cout<<"stop ziooo"<<endl;
+			stop();
+			if(min_left_side(r) < min_right_side(r)){
+				cout<<"stop and turn right"<<endl;
+				turn(-1.0);
+				if(front[35] > th_f && front[40] > th_f && front[45] > th_f){
+					cout<<"go on"<<endl;
+					drive(0.7,0.0);
+				}
+			}
+			else{
+				cout<<"stop and turn left"<<endl;
+				turn(1.0);
+				if(front[35] > th_f && front[40] > th_f && front[45] > th_f){
+					cout<<"go on"<<endl;
+					drive(0.7,0.0);
+				}
+			}
+		}
+		else{
+			if(min_left_side(r) < min_right_side(r)){
+				cout<<"turn on right"<<endl;
+				drive(0.7,-1.0);
+			}
+			else{
+				cout<<"turn left"<<endl;
+				drive(0.7,1.0);
+			}
+		}
 	}
 	
-	/*if(min_front(r) > th && min_left_front(r) > th && min_right_front(r) > th){
-		drive(2.0,0.0);
-	}
-	else if(min_front(r) < th && min_left_front(r) > th && min_right_front(r) > th){
-		if(min_left_side(r) < th_s && min_right_side(r) > th_s){
-			drive(0.0,0.4);
-		}
-		else if(min_left_side(r) > th_s && min_right_side(r) < th_s){
-			drive(0.0,-0.4);
-		}
-		else{
-			drive(0.0,0.4);
-		}
-	}
-	else if(min_front(r) > th && min_left_front(r) > th && min_right_front(r) < th){
-		drive(0.0,-0.4);
-	}
-	else if(min_front(r) > th && min_left_front(r) < th && min_right_front(r) > th){
-		drive(0.0,0.4);
-	}
-	else if(min_front(r) < th && min_left_front(r) > th && min_right_front(r) < th){
-		drive(0.0,-0.4);
-	}
-	else if(min_front(r) < th && min_left_front(r) < th && min_right_front(r) > th){
-		drive(0.0,0.4);
-	}
-	else if(min_front(r) < th && min_left_front(r) < th && min_right_front(r) < th){
-		if(min_left_side(r) < th_s && min_right_side(r) > th_s){
-			drive(0.0,0.4);
-		}
-		else if(min_left_side(r) > th_s && min_right_side(r) < th_s){
-			drive(0.0,-0.4);
-		}
-		else{
-			drive(0.0,0.4);
-		}
-	}
-	else if(min_front(r) > th && min_left_front(r) < th && min_right_front(r) < th){
-		drive(0.8,0.0);
-	}*/
 }
+
 
 void robotCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	
+	cout<<" th = "<<th<<endl;
+
 	for(int i = 0; i < 720 ; i++){
 		ranges[i] = msg -> ranges[i]; 
 		//cout<< "the front range is : " << ranges[i] << endl;
@@ -231,6 +216,8 @@ void robotCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	cout<<"min left side : "<<min_left_side(ranges) <<endl;
 	cout<<"min_right_side : "<< min_right_side(ranges) <<endl;
 	cout<<"front : "<<front[40]<<endl;
+	cout<<"front right : "<<front[35]<<endl;
+	cout<<"front left : "<<front[45]<<endl;
 	cout<<"--------------------------------------------"<<endl;
 	
 	avoid_obstacle(ranges);
