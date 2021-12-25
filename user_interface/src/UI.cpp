6fc2b81381915105d21e6987d47503d29bbf6313
GabrieleRusso11 <93495918+GabrieleRusso11@users.noreply.github.com>
2021-12-25 12:16:21 +0100

    Add files via upload

--- user_interface/src/UI.cpp
+++ user_interface/src/UI.cpp
@@ -4,14 +4,20 @@
 #include "robot_controller/Velocity.h"
 using namespace std;
 
-
-char command;
-
 int main(int argc, char ** argv){
 
+    // node initialization
     ros::init(argc, argv, "User_interface");
+
+    // the NodeHandle is used for handling the communication with the ROS system
+	// ( it is an object wich rapresents the ROS node ).
     ros::NodeHandle n;
+
+    // this creates a client that sends a Reset position request to the /reset_position service
     ros::ServiceClient client_reset = n.serviceClient<std_srvs::Empty>("/reset_positions");
+
+    // this creates a client that sends a Change velocity request to the /velocity_control
+    // custom service
     ros::ServiceClient client_velocity = n.serviceClient<robot_controller::Velocity>("/velocity_control");
 
     std_srvs::Empty reset;
@@ -19,6 +25,7 @@ int main(int argc, char ** argv){
     
 
     while(ros::ok()){
+
         cout<<endl;
         cout<<"Hi this is the robot user interface, you have the following commands : "<<endl;
         cout<<"-----------------------------------------------------------------------"<<endl;
@@ -28,20 +35,31 @@ int main(int argc, char ** argv){
         cout<<"-- press q if you want to exit."<<endl;
         cout<<"-----------------------------------------------------------------------"<<endl;
 
+        // takes the client_velocity request from the user
         cin>>vel.request.command;
         
+        // this 'if else if' structure checks what is the user request
+        // in order to call the correct serve
         if(vel.request.command == 'r'){
             client_reset.waitForExistence();
-            client_reset.call(reset);
+            client_reset.call(reset); // client_reset call the reset_position server
         }
         else if(vel.request.command == 'i' || vel.request.command == 'd'){
             client_velocity.waitForExistence();
-            client_velocity.call(vel);
+            client_velocity.call(vel); // client_velocity call the velocity_control server
         }
         else if(vel.request.command == 'q'){
             exit(EXIT_SUCCESS);
         }
+        else{
+            cout<<"------------------------------------------------"<<endl;
+            cout<<"Error!! Please write only the characters displayed."<<endl;
+            cout<<"------------------------------------------------"<<endl;
+        }
+
     }
+
+    // it blocks the main thread from exiting until ROS invokes a shutdown.
     ros::spin();
 
     return 0;
